4d64fd59c1046e8ea1f7b877af5bdcf10df89759
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-04-04 21:35:16 -0300

    Update prompt

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app_variables = AppVariables()
 env_variables = EnvironmentVariables()
 
 # Gerando o texto
-prompt = 'Faça para mim um artigo para o linkedin com 2 páginas sobre o tema datetime em Python com as seguintes regras:\n* Gere um texto com exemplos de código para cada página\n* Sua resposta deve ser um json válido\n* Não devem haver quebras de linha\n* A sua resposta deve conter apenas objetos no seguinte formato: [{"title": <coloque o titulo da página> ,"description": <coloque a descrição da página> ,"example": "<coloque o exemplo da página>"}, ...]'
+prompt = 'Faça para mim um artigo para o linkedin com 2 páginas sobre o tema datetime em Python com as seguintes regras:\n* Gere um texto com exemplos de código para cada página\n* Sua resposta deve ser um json válido\n* A sua resposta deve conter apenas objetos no seguinte formato: [{"title": <coloque o titulo da página> ,"description": <coloque a descrição da página> ,"example": "<coloque o exemplo da página>"}, ...]'
 chatgpt = OpenAITextGenerator(api_key=env_variables.api_key)
 
 results = chatgpt.generate_text(prompt)
@@ -21,7 +21,7 @@ pages_content = text_extractor.get_pages()
 # Gerar páginas PDF
 pdf_file = PagePDFGenerator()
 
-pdf_file.add_cover()
+pdf_file.add_cover("datetime em Python")
 
 for page_content in pages_content:
     pdf_file.add_page(page_content)
