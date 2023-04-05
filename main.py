bee4f37ceadb25bf351dd0c2b4b3f4adc0ee2088
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-04-05 14:37:36 -0300

    Fine tune o envirioments

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app_variables = AppVariables()
 env_variables = EnvironmentVariables()
 
 # Gerando o texto
-prompt = 'Faça para mim um artigo para o linkedin com 2 páginas sobre o tema datetime em Python com as seguintes regras:\n* Gere um texto com exemplos de código para cada página\n* Sua resposta deve ser um json válido\n* A sua resposta deve conter apenas objetos no seguinte formato: [{"title": <coloque o titulo da página> ,"description": <coloque a descrição da página> ,"example": "<coloque o exemplo da página>"}, ...]'
+prompt = f'Faça para mim um artigo para o LinkedIn com {app_variables.number_of_pages} páginas sobre o tema "{app_variables.post_topic}" com as seguintes regras:\n* Gere um texto com exemplos de código para cada página\n* Sua resposta deve ser um JSON válido\n* A sua resposta deve conter apenas objetos no seguinte formato: [{{"title": "{{coloque o título da página}}", "description": "{{coloque a descrição da página}}", "example": \'{{coloque o exemplo da página}}\'}}]'
 chatgpt = OpenAITextGenerator(api_key=env_variables.api_key)
 
 results = chatgpt.generate_text(prompt)
@@ -21,11 +21,11 @@ pages_content = text_extractor.get_pages()
 # Gerar páginas PDF
 pdf_file = PagePDFGenerator()
 
-pdf_file.add_cover("datetime em Python")
+pdf_file.add_cover(app_variables.post_topic)
 
 for page_content in pages_content:
     pdf_file.add_page(page_content)
 
 pdf_file.add_back_cover()
 
-pdf_file.generate_pdf("outputs/publicação.pdf")
+pdf_file.generate_pdf(f"outputs/{app_variables.post_topic}.pdf")
