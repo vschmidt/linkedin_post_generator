16a76959230df5e77a0e9da9230f97ba0efde1a9
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-04-05 14:38:31 -0300

    Remove base_colors config

--- src/settings/app.py
+++ src/settings/app.py
@@ -2,8 +2,5 @@ from pydantic import BaseSettings, Field
 
 
 class AppVariables(BaseSettings):
-    post_topic: str = Field("try-except em Python", description="Your post topic")
+    post_topic: str = Field("datetime em Python", description="Your post topic")
     number_of_pages: int = Field(2, description="Number of pages in post")
-    base_colors: str = Field(
-        "#0077c2 #1dcaff #2c98f0 #3498db", description="Colors used in post"
-    )
